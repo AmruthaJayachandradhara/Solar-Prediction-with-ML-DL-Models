b4145eb547529d257afac21f64beac6aaefe2563
Yonshim <yonathan.shimelis@gwu.edu>
2026-03-12 00:32:47 -0400

    Went through a few more steps in pipeline stage

--- Radekhiv_Shallow_ML_Pipeline.py
+++ Radekhiv_Shallow_ML_Pipeline.py
@@ -29,7 +29,7 @@ if df['Datetime'].is_monotonic_increasing:
 
 '''Recall that the dataset had a large gap near the end of the dataset from 11/22/2023(5pm)-01/30/2024(9:00am). This has to be addressed before splitting'''
 #Make sure period of time in the dataset doesn't have massive gaps or holes
-print(df['Datetime'].diff(periods=1).value_counts()) #Mostly 1 hour or 2 hour gaps, but there is one large 68 day gap
+print(df['Datetime'].diff(periods=1).value_counts()) #This code was run in the cleaning file, it looks different here since night time hours were largely removed, so gaps look more prevalent now
 #The 68 day gap is from 11/2023 to 1/30/2024, one way around this is to just have the period of observation not include this gap
 
 # %%----------------------------------------------------------------------Splitting the dataset into training and testing sets---------------------------------------------------------------------------------##
@@ -50,22 +50,59 @@ df_train, df_test = train_test_split(df, test_size=0.15, random_state = 42)
 df_train, df_val = train_test_split(df_train, test_size = 0.1765, random_state = 42) 
 
 #Check the shape of the splits for df
-print(df_train.shape)
-print(df_val.shape)
-print(df_test.shape)
+print(df_train.shape) #4880 x 24
+print(df_val.shape) #1046 x 24
+print(df_test.shape) #1046 x 24
 
 #%%-------------------------------------------------------------------Handle uncommon features-------------------------------------------------------------------------------------------
-
 '''No uncommmon features since the dataset was already cleaned and came in one set unlike kaggle datasets that come in one set training one set testing'''
 '''The shape horizontally/feature wise is also the same among all so we know there are no uncommon features'''
 
+#Double check for safety
+
+columns_train = df_train.columns
+columns_val = df_val.columns
+columns_test = df_test.columns
+
+uncommon_feats_train_val = []
+for col in columns_train: 
+    if col not in columns_val:
+        uncommon_feats_train_val.append(col)
+print(uncommon_feats_train_val) #Returned empty list, so no uncommon features between train and val
+
+uncommon_feats_train_test = []
+for col in columns_train:
+    if col not in columns_test:
+        uncommon_feats_train_test.append(col)
+print(uncommon_feats_train_test) #Returned empty list again so good on this end
+
+uncommon_feats_test_val = []
+for col in columns_test:
+    if col not in columns_val:
+        uncommon_feats_test_val.append(col)
+print(uncommon_feats_test_val) #Empty again
+
+
 
 #%%-------------------------------------------------------------------Handle identifiers---------------------------------------------------------------------------------------------
+#Check if there are identifiers in the dataset
+print(df_train.columns)
+print(df_val.columns)
+print(df_test.columns)
 
+'''Next two loops are ran on df intentionally since the splits are all the same shape horizontally and uncommon features were checked'''
+#Check what the index is of the dataframe
+print(df.index) #Returns rangeindex so the index isn't an identifier that's mixed with features
 
+#Loop to check if any columns look like identifiers
+df_columns = df.columns
+for col in df_columns: 
+    if df[col].nunique() == len(df): 
+        print(df[col]) #It only shows sunheight and datetime to be fitting this critera, sunheight was an engineered feature using datetime and pvlib library in the cleaning and EDA code file
 
-
+'''All good for next step'''
 #%%-------------------------------------------------------------------Handle date time variables---------------------------------------------------------------------------------------------
+#For ML models the components of datetime have to be extracted and used
 
 
 
